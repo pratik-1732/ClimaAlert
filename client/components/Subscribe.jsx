ffb2e8272b7c2712693ea1f05a124f1b4a6b39c6
Pratik <patilpratikp035@gmail.com>
2025-06-18 13:06:01 +0530

    added name field

--- client/components/Subscribe.jsx
+++ client/components/Subscribe.jsx
@@ -11,11 +11,24 @@ const Subscribe = () => {
           <h4 className="text-lg font-bold mb-4">Don't miss out!</h4>
           <p>Join thousands of subscribers who trusts us.</p>
         </div>
-        <div className="">
-          <input
-            className="border-2 rounded-lg border-blue-300 outline-none px-4 py-1 mr-5 bg-transparent text-white text-sm font-bold"
-            type="mobileNum" placeholder="Enter Mobile No. "
-          />
+        <div className="flex gap-4 items-center">
+          <div className="flex flex-col gap-3">
+            <input
+              className="border-2 rounded-lg border-blue-300 outline-none px-4 py-1 mr-5 bg-transparent text-white text-sm font-bold"
+              type="name"
+              placeholder="Enter Your Name "
+            />
+            <input
+              className="border-2 rounded-lg border-blue-300 outline-none px-4 py-1 mr-5 bg-transparent text-white text-sm font-bold"
+              type="mobileNum"
+              placeholder="Enter Mobile No. "
+            />
+            <input
+              className="border-2 rounded-lg border-blue-300 outline-none px-4 py-1 mr-5 bg-transparent text-white text-sm font-bold"
+              type="pincode"
+              placeholder="Enter Pincode "
+            />
+          </div>
           <button className="rounded-lg px-6 py-2 bg-blue-800 text-white text-sm font-medium cursor-pointer outline-none">
             Subscribe
           </button>

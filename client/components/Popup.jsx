7252b63af58b86e6c61ad55e443a0b27be6c4adb
Pratik <patilpratikp035@gmail.com>
2025-06-18 14:01:22 +0530

    popup msg component added

---
+++ client/components/Popup.jsx
@@ -0,0 +1,18 @@
+// src/components/Popup.jsx
+import React from "react";
+
+const Popup = ({ message, onClose }) => {
+  return (
+    <div className="fixed top-1/3 left-1/2 transform -translate-x-1/2 bg-white border-2 border-blue-500 text-black px-8 py-6 rounded-lg shadow-lg z-50">
+      <p className="mb-4 font-semibold">{message}</p>
+      <button
+        onClick={onClose}
+        className="bg-blue-500 text-white px-4 py-2 rounded"
+      >
+        OK
+      </button>
+    </div>
+  );
+};
+
+export default Popup;

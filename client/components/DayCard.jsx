84a432a2c92da2ea5fab81387c2a4c9f584df1cf
Pratik <patilpratikp035@gmail.com>
2025-06-20 20:21:39 +0530

    added seven day  info

--- client/components/DayCard.jsx
+++ client/components/DayCard.jsx
@@ -1,16 +1,35 @@
 import React from "react";
 
-const DayCard = () => {
+const DayCard = ({ data, isLast }) => {
+  const {
+    date,
+    day: {
+      condition: { icon, text },
+      maxtemp_c,
+      mintemp_c,
+      daily_chance_of_rain,
+    },
+  } = data;
+
   return (
-    <div className="mb-2 px-4 bg-transparent py-2 border-b-1 border-blue-300 flex justify-between text-sm font-bold text-white">
+    <div
+      className={`mb-2 px-4 bg-transparent py-2 ${
+        !isLast ? "border-b-1 border-blue-300" : ""
+      } flex justify-between text-sm font-bold text-white`}
+    >
       <div className="flex gap-4">
-        <h3>Today</h3>
-        <span className="dot  bg-pink-600 h-8 w-8 rounded-[50%] ml-10"></span>
-        <h3>Sunny</h3>
+        <h3>{date}</h3>
+        <img
+          className="dot h-10 w-10 border-1 border-blue-300 p-1 rounded-[50%] ml-10"
+          src={icon}
+        ></img>
+        <h3>{text}</h3>
       </div>
       <div className="flex gap-5">
-        <h4>10%</h4>
-        <h3>23°/32°</h3>
+        <h4>{daily_chance_of_rain}%</h4>
+        <h3>
+          {mintemp_c}°c/{maxtemp_c}°c
+        </h3>
       </div>
     </div>
   );
--- client/components/SevenDay.jsx
+++ client/components/SevenDay.jsx
@@ -1,18 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import DayCard from "./DayCard";
 
-const SevenDay = () => {
+const SevenDay = ({ data }) => {
+  if (!data) return console.log("data not recieved in SevenDay.jsx");
+
+  const [forecastArr, setForecastArr] = useState([]);
+
+  useEffect(() => {
+    const {
+      forecast: { forecastday },
+    } = data;
+
+    setForecastArr(forecastday);
+  }, [data]);
+
   return (
     <div className="flex flex-col m-20">
       <h2 className="text-2xl font-bold text-white mb-5">7-Day Forecast</h2>
       <div className="bg-blue-400 rounded-2xl p-4">
-        <DayCard />
-        <DayCard />
-        <DayCard />
-        <DayCard />
-        <DayCard />
-        <DayCard />
-        <DayCard />
+        {forecastArr.map((ele, index) => {
+          return (
+            <DayCard
+              key={index}
+              data={ele}
+              isLast={index === forecastArr.length - 1}
+            />
+          );
+        })}
       </div>
     </div>
   );

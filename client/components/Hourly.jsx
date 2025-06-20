e7c1182849ce901bcc088a8e33d8948552a73cd4
Pratik <patilpratikp035@gmail.com>
2025-06-20 22:42:08 +0530

    added code for adding data for upcoming hrs

--- client/components/Hourly.jsx
+++ client/components/Hourly.jsx
@@ -20,14 +20,18 @@ const Hourly = ({ data }) => {
     }
     return color;
   };
-
   const colorArray = Array.from({ length: 24 }, generateRandomHexColor);
+
+  const currentEpoch = Math.floor(Date.now() / 1000);
+  const upcomingHours = hourArr.filter(
+    (hour) => hour.time_epoch > currentEpoch
+  );
 
   return (
     <div className="flex flex-col sm:px-10 lg:px-20 my-10  w-full">
       <h2 className="text-2xl font-bold text-white mb-5">Hourly Forecast</h2>
       <div className="p-7  bg-blue-400 rounded-2xl flex gap-5 flex-wrap">
-        {hourArr.map((ele, index) => {
+        {upcomingHours.map((ele, index) => {
           return (
             <HourCard data={ele} key={index} bgColor={colorArray[index]} />
           );
